ef42bff18c02ee0e3abc705bf6ec422d02799c51
Asahi Kawaguchi <e205716@ie.u-ryukyu.ac.jp>
2023-11-17 13:50:19 +0900

    rep3

--- app/src/main/java/jp/ac/uryukyu/ie/e205716/Enemy.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e205716/Enemy.java
@@ -2,54 +2,67 @@ package jp.ac.uryukyu.ie.e205716;
 
 /**
  * 敵クラス。
- *  String name; //敵の名前
- *  int hitPoint; //敵のHP
- *  int attack; //敵の攻撃力
- *  boolean dead; //敵の生死状態。true=死亡。
- * Created by tnal on 2016/11/13.
  */
 public class Enemy {
-    public String name;
-    public int hitPoint;
-    public int attack;
-    public boolean dead;
+    private String name; // 敵の名前
+    private int hitPoint; // 敵のHP
+    private int attack; // 敵の攻撃力
+    private boolean dead; // 敵の生死状態。true=死亡。
 
-    /**
-     * コンストラクタ。名前、最大HP、攻撃力を指定する。
-     * @param name モンスター名
-     * @param maximumHP モンスターのHP
-     * @param attack モンスターの攻撃力
-     */
-    public Enemy (String name, int maximumHP, int attack) {
+    public Enemy(String name, int maximumHP, int attack) {
         this.name = name;
-        hitPoint = maximumHP;
+        this.hitPoint = maximumHP;
         this.attack = attack;
-        dead = false;
-        System.out.printf("%sのHPは%d。攻撃力は%dです。\n", name, maximumHP, attack);
+        this.dead = false;
+        System.out.printf("%sのHPは%d。攻撃力は%dです。\n", this.name, maximumHP, attack);
     }
 
-    /**
-     * Heroへ攻撃するメソッド。
-     * attackに応じて乱数でダメージを算出し、hero.wounded()によりダメージ処理を実行。
-     * @param hero 攻撃対象
-     */
-    public void attack(Hero hero){
-        int damage = (int)(Math.random() * attack);
-        System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, hero.name, damage);
-        hero.wounded(damage);
-    }
-
-    /**
-     * 自身へ攻撃されたときのダメージ処理をするメソッド。
-     * 指定されたダメージを hitPoint から引き、死亡判定を行う。
-     * @param damage 受けたダメージ
-     */
-    public void wounded(int damage){
-        hitPoint -= damage;
-        if( hitPoint < 0 ) {
-            dead = true;
-            System.out.printf("モンスター%sは倒れた。\n", name);
+    public void attack(Hero hero) {
+        if (!this.dead && !hero.isDead()) {
+            int damage = (int)(Math.random() * this.attack);
+            System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", this.name, hero.getName(), damage);
+            hero.wounded(damage);
         }
     }
 
+    public void wounded(int damage){
+        this.hitPoint -= damage;
+        if (this.hitPoint < 0) {
+            this.dead = true;
+            System.out.printf("モンスター%sは倒れた。\n", this.name);
+        }
+    }
+
+    // Getter and Setter methods
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getHitPoint() {
+        return hitPoint;
+    }
+
+    public void setHitPoint(int hitPoint) {
+        this.hitPoint = hitPoint;
+    }
+
+    public int getAttack() {
+        return attack;
+    }
+
+    public void setAttack(int attack) {
+        this.attack = attack;
+    }
+
+    public boolean isDead() {
+        return dead;
+    }
+
+    public void setDead(boolean dead) {
+        this.dead = dead;
+    }
 }
--- app/src/main/java/jp/ac/uryukyu/ie/e205716/Hero.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e205716/Hero.java
@@ -2,17 +2,17 @@ package jp.ac.uryukyu.ie.e205716;
 
 /**
  * ヒーロークラス。
- *  String name; //敵の名前
- *  int hitPoint; //敵のHP
- *  int attack; //敵の攻撃力
- *  boolean dead; //敵の生死状態。true=死亡。
+ *  String name; // ヒーローの名前
+ *  int hitPoint; // ヒーローのHP
+ *  int attack; // ヒーローの攻撃力
+ *  boolean dead; // ヒーローの生死状態。true=死亡。
  * Created by tnal on 2016/11/13.
  */
 public class Hero {
-    public String name;
-    public int hitPoint;
-    public int attack;
-    public boolean dead;
+    private String name; // ヒーローの名前
+    private int hitPoint; // ヒーローのHP
+    private int attack; // ヒーローの攻撃力
+    private boolean dead; // ヒーローの生死状態。true=死亡。
 
     /**
      * コンストラクタ。名前、最大HP、攻撃力を指定する。
@@ -20,23 +20,25 @@ public class Hero {
      * @param maximumHP ヒーローのHP
      * @param attack ヒーローの攻撃力
      */
-    public Hero (String name, int maximumHP, int attack) {
+    public Hero(String name, int maximumHP, int attack) {
         this.name = name;
-        hitPoint = maximumHP;
+        this.hitPoint = maximumHP;
         this.attack = attack;
-        dead = false;
-        System.out.printf("%sのHPは%d。攻撃力は%dです。\n", name, maximumHP, attack);
+        this.dead = false;
+        System.out.printf("%sのHPは%d。攻撃力は%dです。\n", this.name, maximumHP, attack);
     }
 
     /**
-     * Enemyへ攻撃するメソッド。
-     * attackに応じて乱数でダメージを算出し、hero.wounded()によりダメージ処理を実行。
+     * Enemyへ攻撃するメソッド。死亡していなければ攻撃可能。
+     * attackに応じて乱数でダメージを算出し、e.wounded(damage)によりダメージ処理を実行。
      * @param e 攻撃対象
      */
     public void attack(Enemy e){
-        int damage = (int)(Math.random() * attack);
-        System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, e.name, damage);
-        e.wounded(damage);
+        if (!this.isDead()) {
+            int damage = (int)(Math.random() * this.attack);
+            System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", this.name, e.getName(), damage);
+            e.wounded(damage);
+        }
     }
 
     /**
@@ -45,10 +47,43 @@ public class Hero {
      * @param damage 受けたダメージ
      */
     public void wounded(int damage){
-        hitPoint -= damage;
-        if( hitPoint < 0 ) {
-            dead = true;
-            System.out.printf("勇者%sは道半ばで力尽きてしまった。\n", name);
+        this.hitPoint -= damage;
+        if (this.hitPoint < 0) {
+            this.dead = true;
+            System.out.printf("勇者%sは道半ばで力尽きてしまった。\n", this.name);
         }
+    }
+
+    // Getter and Setter methods
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getHitPoint() {
+        return hitPoint;
+    }
+
+    public void setHitPoint(int hitPoint) {
+        this.hitPoint = hitPoint;
+    }
+
+    public int getAttack() {
+        return attack;
+    }
+
+    public void setAttack(int attack) {
+        this.attack = attack;
+    }
+
+    public boolean isDead() {
+        return dead;
+    }
+
+    public void setDead(boolean dead) {
+        this.dead = dead;
     }
 }
